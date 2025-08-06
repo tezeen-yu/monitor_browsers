97cdcf3ebd96b4584699bea6c8e4f7823213a715
tezeen-yu <tezeenyu@gmail.com>
2025-08-06 11:08:24 +0800

    Update monitorEdge.js

--- monitorEdge.js
+++ monitorEdge.js
@@ -3,13 +3,6 @@ const axios = require("axios");
 
 const version_json = "./version.json";
 
-// const httpsAgent = new HttpsProxyAgent({
-//     host: "15.120.24.30",
-//     port: "8080"
-// })
-
-// const axiosWithProxy = axios.create({ httpsAgent });
-
 const downloadFile = async (url, dest) => {
     var config = {
         responseType: 'stream'
@@ -110,28 +103,6 @@ async function main() {
         fs.writeFileSync(version_json, JSON.stringify(ver_obj, null, "\t"));
         //give the key to jenkins to trigger API and UI automation as there is new version of chrome
         fs.writeFileSync('./isNewVersion_edge_stable.txt', `new_edge_stable_version=${stable_ver}\n`);
-
-    //     let edgePath_win64 = "C:\\BrowserVersion\\Edge_OldVersions\\stable\\win64\\edge_stable_win64_" + stable_ver + ".zip";
-    //     let edgePath_linux64 = "C:\\BrowserVersion\\Edge_OldVersions\\stable\\linux64\\edge_stable_linux64_" + stable_ver_linux64 + ".zip";
-    //     await downloadFile(stable_Dowload_url_win64, edgePath_win64);
-    //     await downloadFile(stable_Dowload_url_linux64, edgePath_linux64);
-    //     checkFile();
-    //     function checkFile() {
-    //         if (!fs.existsSync(edgePath_win64)){
-    //             console.log("---- Warning: The Edge Browser is NOT Exists on the target folder! Will try download again next time! ----");
-    //         } else {
-    //             // write to file
-    //             ver_obj.Edge.previous_stable_version = stable_ver;
-    //             fs.writeFileSync(version_json, JSON.stringify(ver_obj, null, "\t"));
-    //             //give the key to jenkins to trigger API and UI automation as there is new version of chrome
-    //             if (fs.existsSync('./isNewVersion.txt')){
-    //                 fs.appendFileSync('./isNewVersion.txt', `new_edge_stable_version=${stable_ver}\n`);
-    //             }
-    //             else {
-    //                 fs.writeFileSync('./isNewVersion.txt', `new_edge_stable_version=${stable_ver}\n`);
-    //             }
-    //         }
-    //     };
     } else
     {
         console.log(`Not find new edge stable build, current stable build is: ${pre_stable_ver}`)
@@ -144,39 +115,9 @@ async function main() {
         fs.writeFileSync(version_json, JSON.stringify(ver_obj, null, "\t"));
         //give the key to jenkins to trigger API and UI automation as there is new version of chrome
         fs.writeFileSync('./isNewVersion_edge_beta.txt', `new_edge_beta_version=${beta_ver}\n`);
-
-    //     let edgePath_win64 = "C:\\BrowserVersion\\Edge_OldVersions\\beta\\win64\\edge_beta_win64_" + beta_ver + ".zip";
-    //     let edgePath_linux64 = "C:\\BrowserVersion\\Edge_OldVersions\\beta\\linux64\\edge_beta_linux64_" + beta_ver_linux64 + ".zip";
-    //     await downloadFile(beta_Dowload_url_win64, edgePath_win64);
-    //     await downloadFile(beta_Dowload_url_linux64, edgePath_linux64);
-    //     checkFile();
-    //     function checkFile() {
-    //         if (!fs.existsSync(edgePath_win64)){
-    //             console.log("---- Warning: The Edge Browser is NOT Exists on the target folder! Will try download again next time! ----");
-    //         } else {
-    //             // write to file
-    //             ver_obj.Edge.previous_beta_version = beta_ver;
-    //             fs.writeFileSync(version_json, JSON.stringify(ver_obj, null, "\t"));
-    //             //give the key to jenkins to trigger API and UI automation as there is new version of chrome
-    //             if (fs.existsSync('./isNewVersion.txt')){
-    //                 fs.appendFileSync('./isNewVersion.txt', `new_edge_beta_version=${beta_ver}\n`);
-    //             }
-    //             else {
-    //                 fs.writeFileSync('./isNewVersion.txt', `new_edge_beta_version=${beta_ver}\n`);
-    //             }
-    //         }
-    //     };
     }else
     {
         console.log(`Not find new chrome beta build, current beta build is: ${pre_beta_ver}`)
     }
 };
 main().catch(console.error);
-
-// cron.schedule('00 08 * * * ', async () => {
-//     console.log('running a task at 08:00 every day');
-//     requestData();
-// }, {
-//     scheduled: true,
-//     timezone: "Asia/Shanghai"
-// });
